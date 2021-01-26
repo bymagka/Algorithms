df095ec309e10e544e10f0ec653701c9238c8617
bymagka <bymagka@bk.ru>
2021-01-26 19:27:55 +0300

    Task1

---
+++ Homework_Lesson7_TininA/Graphs/Graph.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+/*
+ * Tinin A
+ * 
+ *  1. Написать функции, которые считывают матрицу смежности из файла и выводят ее на экран.
+    2. Написать рекурсивную функцию обхода графа в глубину.
+    3. Написать функцию обхода графа в ширину.
+    4. *Создать библиотеку функций для работы с графами.
+ * 
+ * 
+ * 
+ */
+
+namespace Graphs
+{
+    public class Graph
+    {
+        int[][] matrix;
+
+        public Graph(string filename)
+        {
+            StreamReader matrixReader = new StreamReader(filename);
+
+            int count = int.Parse(matrixReader.ReadLine());
+
+            matrix = new int[count][];
+
+            int currentPosition = 0;
+
+            while (!matrixReader.EndOfStream)
+            {
+                
+                matrix[currentPosition] = new int[count];
+
+                string[] lineSymbols = matrixReader.ReadLine().Split(' ');
+
+                for(int i = 0; i < count; i++)
+                {
+                    matrix[currentPosition][i] = int.Parse(lineSymbols[i]);
+                }
+
+                currentPosition++;
+            }
+
+            matrixReader.Close();
+
+        }
+
+        public void Print()
+        {
+            for(int i = 0; i < matrix.Length; i++)
+            {
+                Console.Write("\n");
+
+                for(int j = 0; j < matrix.Length; j++)
+                {
+                    Console.Write($" {matrix[i][j]}");
+                }
+            }
+        }
+    }
+}
---
+++ Homework_Lesson7_TininA/Homework_Lesson7_TininA_Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Graphs;
+
+namespace Homework_Lesson7_TininA_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //создадим объект графа. Прочитаем файл и выведем его на экран (задание 1)
+            Graph gr = new Graph(@"..\..\matrix.txt");
+            gr.Print();
+
+
+            Console.ReadLine();
+        }
+    }
+}
